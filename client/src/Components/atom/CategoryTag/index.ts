3450615834e23fd9132f018a7e297f11704599cf
leejeongmin <danmin20@khu.ac.kr>
2021-07-27 14:43:46 +0900

    feat: category tag 작성

---
+++ client/src/Components/atom/CategoryTag/index.ts
@@ -0,0 +1,25 @@
+import './style';
+
+import Component from '../../../core/Component';
+import jsx from '../../../core/jsx';
+import './style';
+
+export interface CategoryTagProps {
+  id: string;
+  title: string;
+}
+
+export default class CategoryTag extends Component<CategoryTagProps> {
+  constructor(props: CategoryTagProps) {
+    super(props);
+
+    this.setDom();
+  }
+  render() {
+    return jsx`
+      <div class='category-tag ${this.props.id}'>
+        ${this.props.title}
+      </div>
+    `;
+  }
+}
