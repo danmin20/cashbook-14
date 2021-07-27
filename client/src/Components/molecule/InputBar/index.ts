406296d08b8d9f2bd6754038a2854871497929bb
leejeongmin <danmin20@khu.ac.kr>
2021-07-28 02:20:07 +0900

    feat: input bar 수정

--- client/src/Components/molecule/InputBar/index.ts
+++ client/src/Components/molecule/InputBar/index.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import Component from '../../../core/Component';
 import jsx from '../../../core/jsx';
 import SaveButton from '../../atom/SaveButton';
@@ -5,11 +6,29 @@ import './style';
 
 export interface InputBarProps {}
 
-export default class InputBar extends Component<InputBarProps> {
+export interface InputBarStates {
+  paymentType: 'income' | 'outcome';
+  date: string;
+  category: string | null;
+  content: string | null;
+  payment: string | null;
+  amount: number | null;
+}
+
+export default class InputBar extends Component<InputBarProps, InputBarStates> {
   $saveBtn: Element;
 
   constructor(props: InputBarProps) {
     super(props);
+
+    this.state = {
+      paymentType: 'outcome',
+      date: dayjs(new Date()).format('YYYYMMDD'),
+      category: null,
+      content: null,
+      payment: null,
+      amount: null,
+    };
 
     this.$saveBtn = new SaveButton({
       isActive: true,
@@ -19,12 +38,29 @@ export default class InputBar extends Component<InputBarProps> {
 
     this.setDom();
   }
+
   render() {
+    const { paymentType, category } = this.state;
+
     return jsx`
       <div class='input-bar'>
+
+        <div class='input-bar__buttons'>
+          <div onClick=${() =>
+            this.setState({ paymentType: 'outcome' })} class='${
+      paymentType === 'outcome' ? 'active' : ''
+    }'>지출</div>
+          <div onClick=${() =>
+            this.setState({ paymentType: 'income' })} class='${
+      paymentType === 'income' ? 'active' : ''
+    }'>수입</div>
+        </div>
+
         <div class='input-bar__input'>
           <div class='input-bar__input--label'>일자</div>
-          <div class='input-bar__input--content'>20210720</div>
+          <div class='input-bar__input--content'>
+            ${category || '선택하세요'}
+          </div>
         </div>
         <div class='input-bar__input'>
           <div class='input-bar__input--label'>분류</div>
@@ -32,7 +68,9 @@ export default class InputBar extends Component<InputBarProps> {
         </div>
         <div class='input-bar__input'>
           <div class='input-bar__input--label'>내용</div>
-          <div class='input-bar__input--content'>20210720</div>
+          <div class='input-bar__input--content'>
+            <input style='width: 15rem' placeholder='입력하세요' />
+          </div>
         </div>
         <div class='input-bar__input'>
           <div class='input-bar__input--label'>결제수단</div>
@@ -40,7 +78,10 @@ export default class InputBar extends Component<InputBarProps> {
         </div>
         <div class='input-bar__input'>
           <div class='input-bar__input--label'>금액</div>
-          <div class='input-bar__input--content'>20210720</div>
+          <div class='input-bar__input--content amount'>
+            <div>${paymentType === 'income' ? '+' : '-'}</div>
+            <input placeholder='입력하세요' /> 원
+          </div>
         </div>
         ${this.$saveBtn}
       </div>
