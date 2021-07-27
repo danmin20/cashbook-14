05924ddab9f7030812cb8cf6f0a976b619a6bb8d
leejeongmin <danmin20@khu.ac.kr>
2021-07-27 18:18:20 +0900

    feat: info 작성

--- client/src/Components/molecule/Info/index.ts
+++ client/src/Components/molecule/Info/index.ts
@@ -1,10 +1,57 @@
 import Component from '../../../core/Component';
 import jsx from '../../../core/jsx';
+import SaveButton from '../../atom/SaveButton';
+import './style';
 
-export default class Info extends Component {
+export interface InfoProps {
+  count: number;
+  income: number;
+  outcome: number;
+  checked: ('income' | 'outcome')[];
+}
+
+export default class Info extends Component<InfoProps> {
+  $incomeButton: Element;
+  $outcomeButton: Element;
+
+  constructor(props: InfoProps) {
+    super(props);
+
+    this.$incomeButton = new SaveButton({
+      isActive: this.props.checked.find((i) => i === 'income') ? true : false,
+      type: 'small',
+      onClick: () => {},
+    }).$dom;
+    this.$outcomeButton = new SaveButton({
+      isActive: this.props.checked.find((i) => i === 'outcome') ? true : false,
+      type: 'small',
+      onClick: () => {},
+    }).$dom;
+
+    this.setDom();
+  }
   render() {
+    const { count, income, outcome } = this.props;
+
     return jsx`
-      <div></div>
+      <div class='info-component'>
+        <div>
+          전체 내역 ${count}건
+        </div>
+
+        <div class='buttons'>
+          <div class='buttons__button${
+            this.props.checked.find((i) => i === 'income') ? ' active' : ''
+          }'>
+            ${this.$incomeButton} <span>수입 ${income}</span>
+          </div>
+          <div class='buttons__button${
+            this.props.checked.find((i) => i === 'outcome') ? ' active' : ''
+          }'>
+            ${this.$outcomeButton} <span>지출 ${outcome}</span>
+          </div>
+        </div>
+      </div>
     `;
   }
 }
--- client/src/Pages/Main/index.ts
+++ client/src/Pages/Main/index.ts
@@ -1,22 +1,37 @@
 import Header from '../../Components/atom/Header';
+import Info from '../../Components/molecule/Info';
 import InputBar from '../../Components/molecule/InputBar';
-import Component from '../../core/Component';
+import List, { ListProps } from '../../Components/molecule/list';
+import Component, { PropsType } from '../../core/Component';
 import jsx from '../../core/jsx';
 import './style';
 
-export interface HeaderStates {
+export interface MainStates {
   date: Date;
+  histories: ListProps[];
 }
 
-export default class Main extends Component<HeaderStates> {
+export default class Main extends Component<PropsType, MainStates> {
   $header: Element;
   $inputBar: Element;
+  $info: Element;
+  $historyList: Element;
 
   constructor(props: any) {
     super(props);
 
     this.state = {
       date: new Date(),
+      histories: [
+        {
+          tagId: 'category1',
+          tagTitle: '생활',
+          type: 'large',
+          content: '내용',
+          payment: '신한카드',
+          amount: 10000000,
+        },
+      ],
     };
 
     this.$header = new Header({
@@ -25,17 +40,42 @@ export default class Main extends Component<HeaderStates> {
 
     this.$inputBar = new InputBar({}).$dom;
 
+    this.$info = new Info({
+      count: 23235235,
+      income: 234234,
+      outcome: 41451245,
+      checked: ['income'],
+    }).$dom;
+
+    this.$historyList = document.createElement('div');
+    this.state.histories.forEach((history: ListProps) => {
+      const $history = new List({
+        tagId: history.tagId,
+        tagTitle: history.tagTitle,
+        type: history.type,
+        content: history.content,
+        payment: history.payment,
+        amount: history.amount,
+      }).$dom;
+      this.$historyList.append($history);
+    });
+    console.log(this.$historyList);
+
     this.setDom();
   }
 
   render() {
     return jsx`
-      <div>
+      <div class='main-page'>
         <div class='top'>
           ${this.$header}
           <div class='input-bar-wrapper'>
             ${this.$inputBar}
           </div>
+        </div>
+        <div class='list'>
+          ${this.$info}
+          ${this.$historyList}
         </div>
       </div>
     `;
