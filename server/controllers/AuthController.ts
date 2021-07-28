210ad5ea4f7901782a8c15561065da10da5ed87b
eojin-dev <eojin-dev@naver.com>
2021-07-28 11:47:56 +0900

    style. 주석 추가

--- server/controllers/AuthController.ts
+++ server/controllers/AuthController.ts
@@ -1,7 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 
+// TODO
 async function login(req: Request, res: Response, next: NextFunction) {}
 
+// TODO
 async function register(req: Request, res: Response, next: NextFunction) {}
 
 export const AuthController = {
--- server/types/express.d.ts
+++ server/types/express.d.ts
@@ -1,3 +1,5 @@
+// 참고: https://blog.doitreviews.com/development/2020-03-26-extend-express-request-type-in-typescript/
+
 export {};
 
 declare global {
