578f9b3dcb7898edbecaaa7e303e749d0fce0aed
leejeongmin <danmin20@khu.ac.kr>
2021-07-28 09:56:56 +0900

    fix: shell script 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+echo '---cron 실행---'
+cd /home/ubuntu/cashbook-14
+git pull
 
-cd client && yarn && yarn build
-cd ../server && yarn && yarn start
+lastCommit=$(cat /home/ubuntu/last-commit.info)
+current=$(git rev-parse main)
+
+if [[ $current != $lastCommit ]];
+then
+  cd client && yarn && yarn build
+  cd ../server && yarn && pm2 start
+
+  # 기존 커밋 덮어쓰기
+  echo '---update lastCommit.info---'
+  echo $current > /home/ubuntu/last-commit.info
+fi
