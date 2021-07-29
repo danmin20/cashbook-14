5701d96d80af5d7dcfd88f7493e78f3bd175dd79
eojin-dev <eojin-dev@naver.com>
2021-07-30 00:08:58 +0900

    feat. 원그래프 및 ChartPage 틀

--- client/src/Pages/Chart/index.ts
+++ client/src/Pages/Chart/index.ts
@@ -2,14 +2,17 @@ import Component, { PropsType, StateType } from '../../core/Component';
 import jsx from '../../core/jsx';
 import Header from '../../Components/atom/Header';
 import './style';
+import PieGraph from '../../Components/atom/PieGraph';
 
 export default class Chart extends Component<PropsType, StateType> {
   $header: Element;
+  $pieGraph: Element;
 
   constructor(props: PropsType) {
     super(props);
 
     this.$header = new Header({}).$dom;
+    this.$pieGraph = new PieGraph({}).$dom;
 
     this.setDom();
   }
@@ -20,6 +23,15 @@ export default class Chart extends Component<PropsType, StateType> {
         <div class='top'>
           ${this.$header}
         </div>
+
+        <div class="chart-wrapper">
+          <div class="chart">
+            ${this.$pieGraph}
+            <div>
+              List is here
+            </div>
+        </div>
+
       </div>
     `;
   }
