9182f1fb4e84e7fdd2bf054aaee5c51008a32736
eojin-dev <eojin-dev@naver.com>
2021-08-02 20:41:36 +0900

    feat: ChartContainer

--- client/src/Components/organism/ChartContainer/index.ts
+++ client/src/Components/organism/ChartContainer/index.ts
@@ -1,12 +1,15 @@
+import dayjs from 'dayjs';
 import Component, { PropsType, StateType } from '@/core/Component';
 import jsx from '@/core/jsx';
 import './style';
 import PieGraph from '@/Components/atom/PieGraph';
 import LinearGraph from '@/Components/atom/LinearGraph';
+import List from '@/Components/molecule/list';
 import { getState, subscribe } from '@/core/observer';
 import { dateState } from '@/Model';
 import { getMyPureHistory } from '@/api/me';
-import dayjs from 'dayjs';
+import { returnPrice } from '@/utils/util';
+import { HistoryType } from '@/shared/type';
 
 /*
 TODO:
@@ -19,32 +22,105 @@ export default class ChartContainer extends Component<PropsType, StateType> {
   $pieGraph: Element;
   $linearGraph: Element;
 
-  isLoading: boolean;
+  isOpened: boolean;
+  isLoaded: boolean;
+  groupedData: {
+    category: string;
+    amount: number;
+    color: string;
+    histories: HistoryType[];
+  }[];
+  totalOutcome: number;
+  lastPromise: Promise<any> | null;
 
   constructor(props: PropsType) {
     super(props);
 
-    this.$pieGraph = new PieGraph({}).$dom;
+    this.groupedData = [];
+    this.totalOutcome = 0;
+    this.lastPromise = null;
+
+    this.$pieGraph = new PieGraph({ data: this.groupedData }).$dom;
     this.$linearGraph = new LinearGraph({}).$dom;
 
-    this.isLoading = false;
-    const date = dayjs(getState(dateState) as Date).format('YYYY-MM');
-    getMyPureHistory({ date, type: 'outcome' }).then((histories) => {
-      histories;
-    });
+    this.isOpened = false;
+    this.isLoaded = false;
+    this.handleChangeDate();
+
+    subscribe(dateState, 'chart-container', this.handleChangeDate.bind(this));
 
     this.setDom();
   }
 
+  async handleChangeDate() {
+    this.totalOutcome = 0;
+    const date = dayjs(getState(dateState) as Date).format('YYYY-MM');
+    const promise = getMyPureHistory({ date, type: 'outcome' });
+    this.lastPromise = promise;
+    const histories = await promise;
+    if (this.lastPromise !== promise) {
+      return;
+    }
+    const groupedDataBuilder: {
+      [key: string]: {
+        category: string;
+        amount: number;
+        color: string;
+        histories: HistoryType[];
+      };
+    } = {};
+
+    histories.forEach((history) => {
+      if (!groupedDataBuilder[history.category.id]) {
+        groupedDataBuilder[history.category.id] = {
+          category: history.category.name,
+          amount: 0,
+          color: history.category.color,
+          histories: [],
+        };
+      }
+      groupedDataBuilder[history.category.id].amount += +history.amount;
+      groupedDataBuilder[history.category.id].histories.push(history);
+      this.totalOutcome += +history.amount;
+    });
+
+    this.groupedData = Object.values(groupedDataBuilder).sort(
+      (a, b) => a.amount - b.amount
+    );
+
+    this.$pieGraph = new PieGraph({ data: this.groupedData }).$dom;
+    this.isLoaded = true;
+    this.update();
+  }
+
   render() {
+    if (!this.isLoaded) {
+      return jsx`
+        <div class='chart-loading'>
+          <div class="loader" />
+        </div>
+      `;
+    }
+
     return jsx`
         <div class="chart-wrapper">
             <div class="chart paper">
                 ${this.$pieGraph}
                 <div>
-                <div class="title-active">
-                    이번 달 지출 금액 834,640
-                </div>
+                  <div class="title-active">
+                      이번 달 지출 금액 ${returnPrice(this.totalOutcome)}원
+                  </div>
+                  ${this.groupedData.map((data) => {
+                    return new List({
+                      category: { name: data.category, color: data.color },
+                      listType: 'small',
+                      type: 'outcome',
+                      content: `${Math.floor(
+                        (data.amount / this.totalOutcome) * 100
+                      )}%`,
+                      amount: data.amount,
+                    }).$dom;
+                  })}
                 </div>
             </div>
 
