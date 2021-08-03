df32e336237da61515743d6bdbe5c5832a4f2983
eojin-dev <eojin-dev@naver.com>
2021-08-03 10:45:02 +0900

    feat: PieGraph 설정 파일 분리

---
+++ client/src/Components/atom/PieGraph/defaultConfig.ts
@@ -0,0 +1,13 @@
+import PieGraphConfig from './configType';
+
+const pieGraphDefaultConfig: PieGraphConfig = {
+  canvasWidth: 512,
+  canvasHeight: 512,
+  animationStartPoint: 0,
+  animationEndPoint: 100,
+  animationCoefficient: 32,
+  pieRadius: 128,
+  pieLineWidth: 64,
+};
+
+export default pieGraphDefaultConfig;
--- client/src/Components/atom/PieGraph/index.ts
+++ client/src/Components/atom/PieGraph/index.ts
@@ -1,45 +1,36 @@
+import PieGraphConfig from './configType';
+import pieGraphDefaultConfig from './defaultConfig';
 import './style';
 
-const mock = [
-  {
-    category: '생활',
-    amount: 50000,
-    color: '#4A6CC3',
-  },
-  {
-    category: '의료/건강',
-    amount: 30000,
-    color: '#D092E2',
-  },
-  {
-    category: '쇼핑/뷰티',
-    amount: 10000,
-    color: '#4CB8B8',
-  },
-  {
-    category: '교통',
-    amount: 3009,
-    color: '#E2B765',
-  },
-];
+interface PieGraphData {
+  category: string;
+  amount: number;
+  color: string;
+}
 
 export default class PieGraph {
-  data: any; // 상위 콘텍스트(api 또는 component)의 환경에 따라 변경 가능하기 때문에 우선은 any로 지정
+  data: PieGraphData[];
   $wrapper: HTMLElement;
   $dom: HTMLElement;
   $canvas: HTMLCanvasElement;
   cur: number;
   end: number;
+  config: PieGraphConfig;
 
-  constructor({ data }) {
+  constructor({ data }: { data: PieGraphData[] }) {
     this.data = data;
-    // constructor({}) {
-    // this.data = mock;
+    this.config = pieGraphDefaultConfig;
+    const {
+      canvasWidth,
+      canvasHeight,
+      animationStartPoint,
+      animationEndPoint,
+    } = this.config;
 
     this.$canvas = document.createElement('canvas');
     this.$canvas.className = 'pie';
-    this.$canvas.width = 512;
-    this.$canvas.height = 512;
+    this.$canvas.width = canvasWidth;
+    this.$canvas.height = canvasHeight;
 
     this.$wrapper = document.createElement('div');
     this.$wrapper.className = 'pie-wrapper';
@@ -48,17 +39,20 @@ export default class PieGraph {
     this.$dom = document.createElement('div');
     this.$dom.appendChild(this.$wrapper);
 
-    this.cur = 1;
-    this.end = 100;
-    window.requestAnimationFrame(this.render.bind(this));
+    this.cur = animationStartPoint;
+    this.end = animationEndPoint;
+
+    this.render();
   }
 
   render() {
+    const { pieRadius, pieLineWidth, animationCoefficient } = this.config;
+
     const ctx = this.$canvas.getContext('2d') as CanvasRenderingContext2D;
     ctx.clearRect(0, 0, this.$canvas.width, this.$canvas.height);
     const x = this.$canvas.width / 2;
     const y = this.$canvas.height / 2;
-    const radius = 64 + 64 * (this.cur / this.end);
+    const radius = pieRadius / 2 + (pieRadius / 2) * (this.cur / this.end);
 
     let curAngle = Math.PI * 1.5 - Math.PI * 2 * (1 - this.cur / this.end);
     let fullAngle = (Math.PI * 2 * this.cur) / this.end;
@@ -67,18 +61,18 @@ export default class PieGraph {
       totalAmount += amount;
     }
 
-    // 상위 콘텍스트(api 또는 component)의 환경에 따라 변경 가능하기 때문에 우선은 any로 지정
-    this.data.forEach(({ amount, color }: { amount: any; color: any }) => {
+    this.data.forEach(({ amount, color }) => {
       ctx.beginPath();
       let deltaAngle = (fullAngle * amount) / totalAmount;
       ctx.arc(x, y, radius, curAngle, curAngle + deltaAngle, false);
       curAngle += deltaAngle;
-      ctx.lineWidth = 64 + 32 * (this.cur / this.end);
+      ctx.lineWidth =
+        pieLineWidth / 2 + (pieLineWidth / 2) * (this.cur / this.end);
       ctx.strokeStyle = color;
       ctx.stroke();
     });
 
-    this.cur += (this.end - this.cur) / 32;
+    this.cur += (this.end - this.cur) / animationCoefficient;
     if (this.cur <= this.end) {
       if (this.cur >= this.end - 0.1) {
         this.cur = this.end;
