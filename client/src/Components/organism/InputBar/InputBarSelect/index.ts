1ec7d73c844a426d72fdfc9cde284d4c974b61a0
leejeongmin <danmin20@khu.ac.kr>
2021-08-05 03:57:29 +0900

    refactor: category select

--- client/src/Components/organism/InputBar/InputBarSelect/index.ts
+++ client/src/Components/organism/InputBar/InputBarSelect/index.ts
@@ -7,11 +7,6 @@ import { getState, setState, subscribe } from '@/core/observer';
 import DropDown from '@/Components/molecule/Dropdown';
 import './style';
 
-// interface InputBarSelectProps {
-//   setContent: Function;
-//   content: string | null;
-// }
-
 interface InpuBarSelectState {
   isOpened: boolean;
 }
@@ -34,31 +29,7 @@ class InputBarSelect extends Component<PropsType, InpuBarSelectState> {
   }
 }
 
-export class CategorySelect extends Component<PropsType, StateType> {
-  $incomeCategorySelect: Element = jsx``;
-  $outcomeCategorySelect: Element = jsx``;
-
-  constructor(props: PropsType) {
-    super(props);
-
-    subscribe(historyInputState, 'category-select', this.update.bind(this));
-
-    this.$incomeCategorySelect = new IncomeCategorySelect({}).$dom;
-    this.$outcomeCategorySelect = new OutcomeCategorySelect({}).$dom;
-
-    this.setDom();
-  }
-
-  render() {
-    const paymentType = (getState(historyInputState) as HistoryInputType)
-      .paymentType;
-    return paymentType === 'income'
-      ? this.$incomeCategorySelect
-      : this.$outcomeCategorySelect;
-  }
-}
-
-export class IncomeCategorySelect extends InputBarSelect {
+export class CategorySelect extends InputBarSelect {
   $dropdown: Element = jsx``;
 
   constructor(props: PropsType) {
@@ -75,20 +46,27 @@ export class IncomeCategorySelect extends InputBarSelect {
   willUpdate() {
     const handleSetState = setState(historyInputState);
 
+    const { paymentType } = getState(historyInputState) as HistoryInputType;
+
     this.$dropdown = new DropDown({
       selectType: 'category',
-      items: getState(userState.myIncomeCategories) as CategoryType[],
-      setContent: (category: { id: number; name: string }) =>
+      items: getState(
+        paymentType === 'income'
+          ? userState.myIncomeCategories
+          : userState.myOutcomeCategories
+      ) as CategoryType[],
+      setContent: (category: { id: number; name: string }) => {
+        this.state.isOpened = false;
         handleSetState((oldState: HistoryInputType) => {
           return { ...oldState, category };
-        }),
-      paymentType: 'income',
+        });
+      },
+      paymentType,
     }).$dom;
   }
 
   render() {
     const { isOpened } = this.state;
-    // const { content } = this.props;
 
     const handleOpen = () => {
       this.setState({ isOpened: !this.state.isOpened });
@@ -109,56 +87,6 @@ export class IncomeCategorySelect extends InputBarSelect {
   }
 }
 
-export class OutcomeCategorySelect extends InputBarSelect {
-  $dropdown: Element = jsx``;
-
-  constructor(props: PropsType) {
-    super(props);
-
-    subscribe(
-      userState.myOutcomeCategories,
-      'input-bar-outcomecategories',
-      this.update.bind(this)
-    );
-    subscribe(historyInputState, 'outcomecategories', this.update.bind(this));
-  }
-
-  willUpdate() {
-    const handleSetState = setState(historyInputState);
-
-    this.$dropdown = new DropDown({
-      selectType: 'category',
-      items: getState(userState.myOutcomeCategories) as CategoryType[],
-      setContent: (category: { id: number; name: string }) =>
-        handleSetState((oldState: HistoryInputType) => {
-          return { ...oldState, category };
-        }),
-      paymentType: 'outcome',
-    }).$dom;
-  }
-
-  render() {
-    const { isOpened } = this.state;
-    // const { content } = this.props;
-
-    const handleOpen = () => {
-      this.setState({ isOpened: !this.state.isOpened });
-    };
-
-    const content = (getState(historyInputState) as HistoryInputType).category
-      .name;
-
-    return jsx`
-      <div class='select${content ? '' : ' none'}' onClick=${handleOpen}>
-        ${content || '선택하세요'}
-        <img class='drop-icon' src=${drop} style='transform: ${
-      isOpened ? 'rotate(180deg)' : ''
-    }' />
-        ${isOpened ? this.$dropdown : ''}
-      </div>
-    `;
-  }
-}
 export class PaymentSelect extends InputBarSelect {
   $dropdown: Element = jsx``;
 
@@ -179,19 +107,20 @@ export class PaymentSelect extends InputBarSelect {
     this.$dropdown = new DropDown({
       selectType: 'payment',
       items: getState(userState.myPayments) as PaymentType[],
-      setContent: (payment: { id: number; name: string }) =>
+      setContent: (payment: { id: number; name: string }) => {
+        this.state.isOpened = false;
         handleSetState((oldState: HistoryInputType) => {
           return { ...oldState, payment };
-        }),
+        });
+      },
     }).$dom;
   }
 
   render() {
     const { isOpened } = this.state;
-    // const { content } = this.props;
 
     const handleOpen = () => {
-      this.setState({ isOpened: true });
+      this.setState({ isOpened: !this.state.isOpened });
     };
 
     const content = (getState(historyInputState) as HistoryInputType).payment
